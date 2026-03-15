30d6facc90d11fc030d712c995a7072613b785ce
bs-group-dev <bsgroup.adm@gmail.com>
2026-03-16 05:08:22 +0800

    feat: remove unused import of main_bloc.dart from main_page.dart

--- lib/features/main/views/main_page.dart
+++ lib/features/main/views/main_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:poswork/core/widgets/widgets.dart';
-import 'package:poswork/features/main/bloc/main_bloc.dart';
 import 'package:poswork/features/main/main.dart';
 import 'package:poswork/gen/assets.gen.dart';
 
